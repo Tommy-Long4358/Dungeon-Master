8302fa6934b81c7fad074e7a601b0eba940b7c6f
Crimson5768 <tommy.long7548@gmail.com>
2024-04-26 23:19:00 -0700

    Fixed formatting.

--- Magical.java
+++ Magical.java
@@ -3,21 +3,19 @@ public interface Magical
 {
 	/* String menu for magical attack types */
 	static final String MAGIC_MENU = "1. Magic Missle\n2. Fireball\n3. Thunderclap";
-	
-	
+
 	/** Method for magic missile attack. 
-	 * @param e  Entity being attacked
+	 * @param e  Entity being attacked.
 	 */
 	public String magicMissle(Entity e);
 
 	/** Method for fireball attack. 
-	 * @param e  Entity being attacked
+	 * @param e  Entity being attacked.
 	 */
 	public String fireball(Entity e);
 			
-	/** Method for thunderclap attack 
-	 * @param e  Entity being attacked
+	/** Method for thunderclap attack.
+	 * @param e  Entity being attacked.
 	 */  
 	public String thunderclap(Entity e);
-	
 }
